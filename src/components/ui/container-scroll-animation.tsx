04853eae7be24fca7ff1d8c7eab82c883859595f
makeprodigy <payneparihar13@gmail.com>
2026-05-19 04:59:35 +0530

    style: refine Container Scroll Animation component

--- src/components/ui/container-scroll-animation.tsx
+++ src/components/ui/container-scroll-animation.tsx
@@ -12,6 +12,7 @@ export const ContainerScroll = ({
   const containerRef = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({
     target: containerRef,
+    offset: ["start start", "center center"],
   });
   const [isMobile, setIsMobile] = React.useState(false);
 
