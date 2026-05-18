06b5ee8a454aadd3a3cf464a1bd3b5c3d23c6c7d
makeprodigy <payneparihar13@gmail.com>
2026-05-19 04:59:35 +0530

    refactor: update main layout structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${primaryFont.variable} ${secondaryFont.variable}`}>
-      <body className={`${primaryFont.className} antialiased bg-black`}>
+      <body className={`${primaryFont.className} antialiased bg-zinc-950`}>
         {children}
       </body>
     </html>
